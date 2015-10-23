48cdb769a20427a39d2f7368f1ea6d13f4da1587
galmaverick <draganoiu.george@gmail.com>
2015-10-23 10:45:16 +0200

    0.1 commit

---
+++ Main_Project/FoodClient/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FoodClient.ServiceReference1;
+namespace FoodClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var p = new FoodClient.ServiceReference1.Service1Client();
+            p.GetAllFood();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Main_Project/WcfService1Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WcfService1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange 
+            /*create*/
+            int a = 2;
+            int b = 2;
+            int? result = null;
+            int expectedResult = 4;
+
+            //act
+            result = a + b;            
+
+            //assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
