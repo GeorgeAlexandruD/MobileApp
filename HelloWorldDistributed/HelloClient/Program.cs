89b63d39d48e5653c7406837f36226c4d87f5f34
galmaverick <draganoiu.george@gmail.com>
2015-10-23 11:49:33 +0200

    travis

---
+++ HelloWorldDistributed/HelloClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new HelloWorldServiceRefference.HelloWorldClient();
+            Console.WriteLine("a");
+          string a =  p.GetMessage("asd");
+          Console.WriteLine(a);
+            Console.Read();
+        }
+    }
+}
---
+++ HelloWorldDistributed/HelloWorldClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using HelloWorldClient.HelloWorldService;
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new HelloWordServiceRefference.HelloWorldClient();
+            string s = p.GetMessage("no");
+            Console.WriteLine(s);
+            Console.Read();
+
+        }
+    }
+}
